903f04c6b63dcb0ec0d84bca0d7d3250b0a14179
VictorLagunaR <vitor.v.l.rlaguna@gmail.com>
2022-07-15 23:00:41 -0300

    n sei pq ainda está bugando

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -24,30 +24,3 @@ btnMenu.addEventListener('click', menu)
 btnSubmenu.addEventListener('mouseenter', EntrarSubmenu)
 btnSubmenu.addEventListener('mouseleave', SairSubmenu)
 btnSubmenu.addEventListener('click', submenu)
-
-
-
-var splide = new Splide( '.splide', {
-    updateOnMove: true,
-    cover   : true,
-    type   : 'loop',
-    perPage     : 3,
-    perMove     : 1,
-    focus  : 'center',
-    autoplay: true,
-    pauseOnHover:true,
-    breakpoints:{
-        600:{
-            perPage:1,
-            perMove:1,
-        },
-        1000:{
-            perPage:2,
-            perMove: 2,
-        }
-    }
-    } );
-    splide.mount();
-
-
-
