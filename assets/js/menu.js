a4a314a946e363a4080222dcf25f83247d09e93f
VictorLagunaR <vitor.v.l.rlaguna@gmail.com>
2022-07-19 04:04:43 -0300

    adicionando produtos em bolo e arrumando bug com submenu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,26 +1,37 @@
 const btnMenu = document.querySelector('#botao__menu')
 const btnSubmenu = document.querySelector('#produtos')
+const produtos = document.querySelector('#produtos')
 
-function menu() {
-    const nav = document.querySelector('#cabecalho__navegacao')
+//função para a versão mobile
+    function menu() {
+        const nav = document.querySelector('#cabecalho__navegacao')
 
-    nav.classList.toggle('ativo')
-}
+        nav.classList.toggle('ativo')
+        btnSubmenu.removeEventListener('mouseenter', EntrarSubmenu)
+        btnSubmenu.removeEventListener('mouseleave', SairSubmenu)
+        btnSubmenu.addEventListener('click', submenu)
+
+    }
+
+    function EntrarSubmenu() {
+        produtos.classList.toggle('ativo')
+
+        
+    }
+    
+    function SairSubmenu() {
+        produtos.classList.toggle('ativo')
+        
+        
+    }
 
 function submenu() {
-    const produtos = document.querySelector('#produtos')
+
     produtos.classList.toggle('ativo')
 }
 
-function EntrarSubmenu() {
-    produtos.classList.toggle('ativo')
-}
 
-function SairSubmenu() {
-    produtos.classList.toggle('ativo')
-}
 
 btnMenu.addEventListener('click', menu)
 btnSubmenu.addEventListener('mouseenter', EntrarSubmenu)
 btnSubmenu.addEventListener('mouseleave', SairSubmenu)
-btnSubmenu.addEventListener('click', submenu)
---
+++ assets/js/produtos.js
@@ -0,0 +1,8 @@
+const categorias = document.querySelectorAll('[data-categoria]')
+const categoriaOpcaoUm = document.querySelector('[data-opcao=um]')
+
+function apenasOpcaoUm() {
+    
+}
+
+categoriaOpcaoUm.addEventListener('click', apenasOpcaoUm)
