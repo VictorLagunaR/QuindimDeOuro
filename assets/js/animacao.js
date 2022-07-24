6446bd0c490981b69a9c4075d4ef95dce678d7ac
VictorLagunaR <vitor.v.l.rlaguna@gmail.com>
2022-07-24 15:59:31 -0300

    deletando arquivos inuteis e criando o estilo das páginas de cada produto

--- assets/js/animacao.js
+++ assets/js/animacao.js
@@ -1,13 +1,36 @@
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
 const target = document.querySelectorAll('[data-animacao]')
 const animationClass = 'animacao'
 
 function animacaoScroll() {
-    const windowTop = window.scrollY + (window.innerHeight*0.82)
+    const windowTop = window.scrollY + window.innerHeight
     target.forEach(function(element){
         if ((windowTop) >element.offsetTop ) {
             element.classList.add(animationClass)
         }
+
     })
+}
+
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+        animacaoScroll();
+    },40))
 }
 
 animacaoScroll()
--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,8 +1,18 @@
+const botaoCategoriaUm = document.getElementsByClassName('.categorias__botao, [0]')
 const categorias = document.querySelectorAll('[data-categoria]')
-const categoriaOpcaoUm = document.querySelector('[data-opcao=um]')
+const produtosContainer = document.getElementsByClassName('produtos__container')
 
-function apenasOpcaoUm() {
+function opcao() {
+    console.log('a')
+    for (let categorias = 0; categorias < categorias.length; categorias++) {
+        console.log("a")
+        if (produtosContainer.dataset.categoria == categorias.dataset.opcao) {
+        
+            console.log("produtosContainer")
+        }
+    
+    }
     
 }
 
-categoriaOpcaoUm.addEventListener('click', apenasOpcaoUm)
+botaoCategoriaUm.addEventListener('click', opcao)
