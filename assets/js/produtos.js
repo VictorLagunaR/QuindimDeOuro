f302e46b37565f60ef5c1df0dff42e54fab90d20
lagunalr <matheus.laguna.r@gmail.com>
2022-08-03 14:04:07 -0300

    adicionando função para mostrar itens especificos

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,18 +1,76 @@
-const botaoCategoriaUm = document.getElementsByClassName('.categorias__botao, [0]')
-const categorias = document.querySelectorAll('[data-categoria]')
-const produtosContainer = document.getElementsByClassName('produtos__container')
 
-function opcao() {
-    console.log('a')
-    for (let categorias = 0; categorias < categorias.length; categorias++) {
-        console.log("a")
-        if (produtosContainer.dataset.categoria == categorias.dataset.opcao) {
+//recebendo os botões da parte de categoria
+const mostrar = document.querySelector('[data-todos]')
+const opcaoUm = document.querySelector('[data-opcao=um]')
+const opcaoDois = document.querySelector('[data-opcao=dois]')
+const opcaoTres = document.querySelector('[data-opcao=tres]')
+const opcaoQuatro = document.querySelector('[data-opcao=quatro]')
+
+//recebendo todos os itens categorizados
+const categorias = document.querySelectorAll('[data-categoria]')
+
+function desaparecendoTodoAsOpcoes() {
+    const opcoes = document.querySelectorAll('[data-categoria]')
+    for (let x = 0; x < opcoes.length; x++) {
+        opcoes[x].style.display="none";
+    }
+}
+
+function mostrarTodos() {
+    const opcoes = document.querySelectorAll('[data-categoria]')
+    for (let x = 0; x < opcoes.length; x++) {
+        opcoes[x].style.display="flex";
         
-            console.log("produtosContainer")
-        }
-    
+    }
+}
+
+function opcao1() {
+
+    desaparecendoTodoAsOpcoes();
+
+    const opcoes1 = document.querySelectorAll('[data-categoria = um]');
+    for (let x = 0; x < opcoes1.length; x++) {
+        opcoes1[x].style.display = "flex";
+        
     }
     
 }
 
-botaoCategoriaUm.addEventListener('click', opcao)
+function opcao2() {
+    
+    desaparecendoTodoAsOpcoes();
+
+    const opcoes2 = document.querySelectorAll('[data-categoria = dois]')
+    for (let x = 0; x < opcoes2.length; x++) {
+        opcoes2[x].style.display = "flex";
+        
+    }
+}
+
+function opcao3() {
+    
+    desaparecendoTodoAsOpcoes();
+
+    const opcoes3 = document.querySelectorAll('[data-categoria = tres]')
+    for (let x = 0; x < opcoes3.length; x++) {
+        opcoes3[x].style.display = "flex";
+        
+    }
+}
+
+function opcao4() {
+    
+    desaparecendoTodoAsOpcoes();
+
+    const opcoes4 = document.querySelectorAll('[data-categoria = quatro]')
+    for (let x = 0; x < opcoes4.length; x++) {
+        opcoes4[x].style.display = "flex";
+        
+    }
+}
+
+mostrar.addEventListener('click', mostrarTodos);
+opcaoUm.addEventListener('click', opcao1);
+opcaoDois.addEventListener('click', opcao2);
+opcaoTres.addEventListener('click', opcao3);
+opcaoQuatro.addEventListener('click', opcao4);
